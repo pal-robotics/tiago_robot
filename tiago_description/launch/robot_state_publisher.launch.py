21258130dcae9b672bff4443e437c07f074195f5
Jordan Palacios <jordan.palacios@pal-robotics.com>
2021-02-12 14:01:28 +0100

    Formatting

--- tiago_description/launch/robot_state_publisher.launch.py
+++ tiago_description/launch/robot_state_publisher.launch.py
@@ -6,22 +6,22 @@ from typing import List
 from launch import Substitution, SomeSubstitutionsType, LaunchContext
 from typing import Text
 
-from pmb2_description.launch_utils import read_launch_argument 
-from pmb2_description.pmb2_launch_utils import get_tiago_base_hw_arguments 
+from pmb2_description.launch_utils import read_launch_argument
+from pmb2_description.pmb2_launch_utils import get_tiago_base_hw_arguments
 
 
 class TiagoXacroConfigSubstitution(Substitution):
     """
-    Substitution extracts the pmb2 hardware args and passes them as xacro variables
-    Used in launch system
+    Substitution extracts the tiago hardware args and passes them
+    as xacro variables. Used in launch system
     """
 
     def __init__(self) -> None:
         super().__init__()
         """
-    Construct the substitution
-    :param: source_file the original YAML file t
-    """
+        Construct the substitution
+        :param: source_file the original YAML file t
+        """
 
     @property
     def name(self) -> List[Substitution]:
@@ -30,7 +30,8 @@ class TiagoXacroConfigSubstitution(Substitution):
 
     def describe(self) -> Text:
         """Return a description of this substitution as a string."""
-        return 'Parses tiago hardware launch arguments into xacro arguments potat describe'
+        return 'Parses tiago hardware launch arguments into xacro \
+        arguments potat describe'
 
     def perform(self, context: LaunchContext) -> Text:
         """
@@ -39,23 +40,27 @@ class TiagoXacroConfigSubstitution(Substitution):
 
         laser_model = read_launch_argument("laser_model", context)
         rgbd_sensors = read_launch_argument("rgbd_sensors", context)
-        return " laser_model:=" + laser_model + " rgbd_sensors:=" + rgbd_sensors
+        return " laser_model:=" + laser_model + \
+            " rgbd_sensors:=" + rgbd_sensors
+
 
 def generate_launch_description():
-
-    parameters={'robot_description': Command(
-      [
-        ExecutableInPackage(package='xacro', executable="xacro"),
-        ' ',
-        PathJoinSubstitution([FindPackageShare('tiago_description'), 'robots', 'tiago.urdf.xacro']),
-        TiagoXacroConfigSubstitution()
-      ])
+    parameters = {'robot_description': Command(
+        [
+            ExecutableInPackage(package='xacro', executable="xacro"),
+            ' ',
+            PathJoinSubstitution(
+                [FindPackageShare('tiago_description'),
+                 'robots', 'tiago.urdf.xacro']),
+            TiagoXacroConfigSubstitution()
+        ])
     }
 
     rsp = Node(package='robot_state_publisher',
-                                  executable='robot_state_publisher',
-                                  output='both',
-                                  parameters=[parameters])
+               executable='robot_state_publisher',
+               output='both',
+               parameters=[parameters])
+
     return LaunchDescription([
         *get_tiago_base_hw_arguments(laser_model=True,
                                      rgbd_sensors=True,
