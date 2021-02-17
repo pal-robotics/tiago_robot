3e3a9c8a16330484b3675ea1bf1fbd70df053dcb
Jordan Palacios <jordan.palacios@pal-robotics.com>
2021-02-17 09:54:18 +0100

    Launch file for showing the description in rviz2

---
+++ tiago_description/launch/show.launch.py
@@ -0,0 +1,43 @@
+# Copyright (c) 2021 PAL Robotics S.L.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+from launch_pal.include_utils import include_launch_py_description
+
+
+def generate_launch_description():
+
+    robot_state_publisher = include_launch_py_description(
+        'tiago_description', ['launch', 'robot_state_publisher.launch.py'])
+
+    start_joint_pub_gui = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        name='joint_state_publisher_gui',
+        output='screen')
+
+    start_rviz_cmd = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        #       arguments=['-d', rviz_config_file],
+        output='screen')
+
+    return LaunchDescription([
+        robot_state_publisher,
+        start_joint_pub_gui,
+        start_rviz_cmd
+    ])
