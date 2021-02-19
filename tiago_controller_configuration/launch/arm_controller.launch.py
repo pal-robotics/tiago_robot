fc9ee04445c73143ca4c4890a981d6c3baf50a9d
Jordan Palacios <jordan.palacios@pal-robotics.com>
2021-02-19 10:25:33 +0100

    Using controller_manager launch_utils

--- tiago_controller_configuration/launch/arm_controller.launch.py
+++ tiago_controller_configuration/launch/arm_controller.launch.py
@@ -24,46 +24,17 @@
 #  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 #  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-import os
-
 from ament_index_python.packages import get_package_share_directory
 
-from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
+from controller_manager.launch_utils import generate_load_controller_launch_description
 
-from launch_ros.actions import Node
-
-
-# @todo put this somewhere reusable
-# @see https://github.com/ros-controls/ros2_control/issues/320
-def generate_load_controller_launch_description(
-        controller_name, controller_type, pkg_name, controller_params_file):
-
-    pkg_path = get_package_share_directory(pkg_name)
-    param_file_path = os.path.join(pkg_path, controller_params_file)
-
-    spawner = Node(package='controller_manager', executable='spawner.py',
-                   arguments=[controller_name,
-                              '--controller-type', controller_type,
-                              '--controller-manager', LaunchConfiguration(
-                                  'controller_manager_name'),
-                              '--param-file', param_file_path],
-                   output='screen')
-
-    return LaunchDescription([
-        DeclareLaunchArgument(
-            'controller_manager_name', default_value='controller_manager',
-            description='Controller manager node name'
-        ),
-        spawner,
-    ])
+import os
 
 
 def generate_launch_description():
     return generate_load_controller_launch_description(
         controller_name='arm_controller',
         controller_type='joint_trajectory_controller/JointTrajectoryController',
-        pkg_name='tiago_controller_configuration',
         controller_params_file=os.path.join(
+            get_package_share_directory('tiago_controller_configuration'),
             'config', 'arm_controller.yaml'))
