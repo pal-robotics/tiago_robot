6c43ebcb77e409488842a0050be4e36c5a385494
Jordan Palacios <jordan.palacios@pal-robotics.com>
2021-03-05 10:52:18 +0100

    First version of the tiago_bringup.launch.py

---
+++ tiago_bringup/launch/tiago_bringup.launch.py
@@ -0,0 +1,32 @@
+# Copyright (c) 2021 PAL Robotics S.L.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from launch import LaunchDescription
+from launch_pal.include_utils import include_launch_py_description
+
+
+def generate_launch_description():
+    default_controllers = include_launch_py_description(
+        'tiago_controller_configuration',
+        ['launch', 'default_controllers.launch.py'])
+
+    # @TODO: robot state publisher? move it here from tiago_spawn?
+    # @TODO: robot pose publisher
+    # @TODO: tf lookup
+    # @TODO: twist mux
+    # @TODO: dynamic footprint
+    # @TODO: joystick teleop
+
+    return LaunchDescription([
+        default_controllers])
