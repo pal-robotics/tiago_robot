31214da9d50add45432f20583d82e971353eb919
Jordan Palacios <jordan.palacios@pal-robotics.com>
2021-07-26 12:19:53 +0200

    Load robot_description_semantic into play_motion

--- tiago_bringup/launch/play_motion.launch.py
+++ tiago_bringup/launch/play_motion.launch.py
@@ -9,7 +9,8 @@ from launch import Substitution, SomeSubstitutionsType, LaunchContext
 from typing import List
 from typing import Text
 from launch_pal.arg_utils import read_launch_argument
-from tiago_description.tiago_launch_utils import get_tiago_hw_arguments
+from launch_pal.substitutions import LoadFile
+from tiago_description.tiago_launch_utils import get_tiago_hw_arguments, get_tiago_hw_suffix
 
 
 class TiagoXacroConfigSubstitution(Substitution):
@@ -66,6 +67,15 @@ def generate_launch_description():
         ])
     }
 
+    robot_description_semantic_config = LoadFile(
+        [get_package_share_directory("tiago_moveit_config"), "/config/srdf/tiago_",
+         get_tiago_hw_suffix(arm=True, wrist_model=False,
+                             end_effector=True, ft_sensor=True), ".srdf"]
+    )
+    robot_description_semantic = {
+        "robot_description_semantic": robot_description_semantic_config
+    }
+
     # @TODO use the tiago args to correctly load the proper file
     approach_planner = os.path.join(
         get_package_share_directory('tiago_bringup'),
@@ -81,7 +91,12 @@ def generate_launch_description():
     play_motion = Node(package='play_motion',
                        executable='play_motion',
                        output='both',
-                       parameters=[robot_description, approach_planner, motions])
+                       parameters=[
+                           robot_description,
+                           robot_description_semantic,
+                           approach_planner,
+                           motions,
+                       ])
 
     tiago_args = get_tiago_hw_arguments(
         laser_model=True,
