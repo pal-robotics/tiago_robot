bc11c84b4584eee0807654c5e120ddf0c4a83d96
Adria Roig <adria.roig@pal-robotics.com>
2023-03-09 10:38:22 +0100

    Add gravity compensation controller

--- tiago_controller_configuration/launch/default_controllers.launch.py
+++ tiago_controller_configuration/launch/default_controllers.launch.py
@@ -63,6 +63,10 @@ def generate_launch_description():
         'tiago_controller_configuration',
         ['launch', 'arm_controller.launch.py'])
 
+    gravity_compensation_controller_launch = include_launch_py_description(
+            'tiago_controller_configuration',
+            ['launch', 'gravity_compensation_controller.launch.py'])
+
     ld = LaunchDescription()
 
     ld.add_action(get_robot_name('tiago'))
@@ -74,6 +78,7 @@ def generate_launch_description():
     ld.add_action(torso_controller_launch)
     ld.add_action(head_controller_launch)
     ld.add_action(arm_controller_launch)
+    ld.add_action(gravity_compensation_controller_launch)
     ld.add_action(OpaqueFunction(function=launch_end_effector_controller))
 
     return ld
