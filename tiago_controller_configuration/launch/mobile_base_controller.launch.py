4e2daa51ca210ec2b4a2173cd8c9eacb4767a7db
Noel Jimenez <noel.jimenez@pal-robotics.com>
2023-09-20 14:49:02 +0200

    Use wheels calibration if exists

--- tiago_controller_configuration/launch/mobile_base_controller.launch.py
+++ tiago_controller_configuration/launch/mobile_base_controller.launch.py
@@ -15,13 +15,25 @@
 import os
 
 from ament_index_python.packages import get_package_share_directory
+
 from controller_manager.launch_utils import generate_load_controller_launch_description
+from launch_pal.param_utils import merge_param_files
 
 
 def generate_launch_description():
+
+    default_config = os.path.join(
+        get_package_share_directory('tiago_controller_configuration'),
+        'config', 'mobile_base_controller.yaml')
+
+    calibration_config = '/etc/calibration/master_calibration.yaml'
+
+    if os.path.exists(calibration_config):
+        params_file = merge_param_files([default_config, calibration_config])
+    else:
+        params_file = default_config
+
     return generate_load_controller_launch_description(
         controller_name='mobile_base_controller',
         controller_type='diff_drive_controller/DiffDriveController',
-        controller_params_file=os.path.join(
-            get_package_share_directory('tiago_controller_configuration'),
-            'config', 'mobile_base_controller.yaml'))
+        controller_params_file=params_file)
