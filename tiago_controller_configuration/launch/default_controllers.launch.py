a033fbbf7b63515ffbc16efc5b2a5bf62663915d
Noel Jimenez <noel.jimenez@pal-robotics.com>
2024-11-27 08:13:28 +0000

    Remove unused imu broadcaster

--- tiago_controller_configuration/launch/default_controllers.launch.py
+++ tiago_controller_configuration/launch/default_controllers.launch.py
@@ -79,18 +79,6 @@ def declare_actions(
     )
     launch_description.add_action(joint_state_broadcaster)
 
-    # IMU sensor broadcaster
-    imu_sensor_broadcaster = GroupAction(
-        [
-            generate_load_controller_launch_description(
-                controller_name='imu_sensor_broadcaster',
-                controller_params_file=os.path.join(
-                    pkg_share_folder, 'config', 'imu_sensor_broadcaster.yaml'))
-
-        ],
-    )
-    launch_description.add_action(imu_sensor_broadcaster)
-
     # Torso controller
     torso_controller = GroupAction(
         [
